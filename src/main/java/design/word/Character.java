5d6f86aa4cf77a7c5c8edcf670624d768d8bad2d
youngxhui <youngxhui@gmail.com>
2020-06-06 14:03:24 +0800

    完成word上段落 偏移量 阈值的统计

--- src/main/java/design/word/Character.java
+++ src/main/java/design/word/Character.java
@@ -10,19 +10,18 @@ import java.util.Map;
 
 public class Character implements Content {
 
-    private final Map<Object, Integer> characterPool = new HashMap<>();
+    private final Map<Object, String> characterPool = new HashMap<>();
 
     @Override
-    public Map<Object, Integer> analysis(String content) {
+    public Map<Object, String> analysis(String content,int limit) {
         for (int i = 0; i < content.length(); i++) {
             if (!java.lang.Character.isLetter(content.charAt(i))) {
                 continue;
             }
-
             if (characterPool.containsKey(content.charAt(i))) {
-                characterPool.put(content.charAt(i), characterPool.get(content.charAt(i)) + 1);
+                characterPool.put(content.charAt(i), characterPool.get(Integer.parseInt(String.valueOf(content.charAt(i))) + 1));
             } else {
-                characterPool.put(content.charAt(i), 1);
+                characterPool.put(content.charAt(i), String.valueOf(1));
             }
         }
         return characterPool;
