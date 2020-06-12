e85ec480dddb8d9fa7a071fadf825d2e15844717
youngxhui <youngxhui@gmail.com>
2020-06-12 21:17:20 +0800

    生成Sentence类

---
+++ src/main/java/design/word/Sentence.java
@@ -0,0 +1,14 @@
+package design.word;
+
+import java.util.Map;
+
+/**
+ * Create by young on 2020/6/12
+ * Copyright © 2020 young. All rights reserved.
+ */
+public class Sentence implements Content {
+    @Override
+    public Map<Object, String> analysis(String content, int limit) {
+        return null;
+    }
+}
